b0e19eaface9c682a4dd47c572fcac62da0edf96
Miller Prosser <38267557+MillerPr@users.noreply.github.com>
2021-02-02 21:29:47 -0600

    nav.html fixes

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,4 @@
-$(document).ready(function () {
-        // get current URL path and assign 'active' class
-        var pathname = window.location.pathname;
-        $('.navbar-nav > li > a[href="/map.html"]').addClass('active');
-      })
+$(function () {
+      $("#nav-placeholder").load("nav.html");
+    });
+    $("div#title").last().addClass("h1");
