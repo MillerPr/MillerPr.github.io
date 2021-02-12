de48ace41541e167512a6675f69fc1cb2b5ea74f
Miller Prosser <mcprosse@uchicago.edu>
2021-02-11 22:10:42 -0600

    tweak wiki

--- js/wikiAPI.js
+++ js/wikiAPI.js
@@ -1,38 +1,45 @@
 function wikiAPI() {
 
-var searchTerm = document.getElementById('searchTerm').value;
-var connect = new XMLHttpRequest();
-var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=20&gsrsearch=" + searchTerm;
+  var searchTerm = document.getElementById('searchTerm').value;
+  var connect = new XMLHttpRequest();
+  var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=20&gsrsearch=" + searchTerm;
 
-connect.open('GET', url);
+  connect.open('GET', url);
 
-connect.onload = function () {
-  var wikiObject = JSON.parse(this.response);
-  //console.log(wikiObject);
-  console.log(wikiObject.query.pages);
-  var pages = wikiObject.query.pages;
-  for (i in pages) {
-    //basic function
-    /* var newDiv = document.createElement("div");
-    newDiv.setAttribute('class', 'row h4');
-    document.getElementById("wiki").appendChild(newDiv);
-    newDiv.innerText = pages[i].title; */
+  connect.onload = function () {
+    var wikiObject = JSON.parse(this.response);
+    //console.log(wikiObject);
+    console.log(wikiObject.query.pages);
+    var pages = wikiObject.query.pages;
+    for (i in pages) {
+      //basic function
+      /* var newDiv = document.createElement("div");
+      newDiv.setAttribute('class', 'row h4');
+      document.getElementById("wiki").appendChild(newDiv);
+      newDiv.innerText = pages[i].title; */
 
-    //super challenge
-    var pageURL = "https://en.wikipedia.org/?curid="
-    var newAnchor = document.createElement("a");
-    newAnchor.setAttribute('href', pageURL+pages[i].pageid);
-    newAnchor.setAttribute('class', 'd-block');
-    newAnchor.innerText = pages[i].title;
-    document.getElementById("wiki").appendChild(newAnchor);
-  };
-}
-connect.send();
+      //super challenge
+      var pageURL = "https://en.wikipedia.org/?curid="
+      var newAnchor = document.createElement("a");
+      newAnchor.setAttribute('href', pageURL+pages[i].pageid);
+      newAnchor.setAttribute('class', 'd-block');
+      newAnchor.innerText = pages[i].title;
+      document.getElementById("wiki").appendChild(newAnchor);
+    };
+  }
+  connect.send();
 
-//SUPER CHALLENGE
+  //SUPER CHALLENGE
   //Display links to the Wikipedia HTML pages as results.
   //Use the pageid property in the JSON file.
   //The base URL for loading Wikipedia pages by pageid is the following:
-    //https://en.wikipedia.org/?curid=
-    //Add the pageid to the end of the URL.
+  //https://en.wikipedia.org/?curid=
+  //Add the pageid to the end of the URL.
+}
+
+function wikiEvent() {
+  const wikiInput = document.getElementById("searchTerm");
+  const wikiButton = document.getElementById("wikiButton");
+  wikiInput.addEventListener("sbumit", wikiAPI);
+  wikiButton.addEventListener("click", wikiAPI);
 }
