44d6ad2b806a5c14598ea25474450bd825028cc7
Miller Prosser <38267557+MillerPr@users.noreply.github.com>
2021-03-24 16:59:34 -0500

    update large set display

--- js/loadLargeSet.js
+++ js/loadLargeSet.js
@@ -29,22 +29,55 @@ function listItems(sourceXML) {
   var items = sourceXML.getElementsByTagName('items')[0];
   var spatialUnits = items.childNodes;
   for (var i = 0; i < spatialUnits.length; i++) {
-    var itemName = spatialUnits[i].childNodes[0].childNodes[0].textContent;
-    /* var itemAliasNode = spatialUnits[i].childNodes[0].childNodes;
-    var itemAliases = []
-    for (var j = 1; j < itemAliasNode.length; j++){
-      itemAliases.push(itemAliasNode[j].innerHTML);
-    }; */
+    //Create a new row
     var tr = document.createElement('tr');
     tr.className = 'ochreTableRows';
-    //Set unique attributes per row.
     tr.id = 'row_'+i;
     document.getElementById('ochreTableBody').appendChild(tr);
+
+    //Create Name column
+    var itemName = spatialUnits[i].childNodes[0].childNodes[0].textContent;
     var td = document.createElement('td');
     td.className = 'Name';
     td.id = 'td_row_'+i;
     td.textContent = itemName;
     document.getElementById('row_'+i).appendChild(td);
+
+    //Create Alias column
+    var itemAliasNode = spatialUnits[i].childNodes[0].childNodes;
+    var itemAliases = []
+    for (var j = 1; j < itemAliasNode.length; j++){
+      itemAliases.push(itemAliasNode[j].innerHTML);
+    };
+    var td2 = document.createElement('td');
+    td2.className = 'Alias';
+    td2.id = 'td2_row_'+i;
+    td2.textContent = itemAliases.join(', ');
+    document.getElementById('row_'+i).appendChild(td2);
+
+    //Description
+    var itemDescription = spatialUnits[i].childNodes[2].textContent;
+    var td3 = document.createElement('td');
+    td3.className = 'Description';
+    td3.id = 'td3_row_'+i;
+    td3.textContent = itemDescription;
+    document.getElementById('row_'+i).appendChild(td3);
+
+    //Context
+    var itemContext = spatialUnits[i].childNodes[1].textContent;
+    var td4 = document.createElement('td');
+    td4.className = 'Context';
+    td4.id = 'td4_row_'+i;
+    td4.textContent = itemContext;
+    document.getElementById('row_'+i).appendChild(td4);
+
+    //Period
+    var itemPeriod = spatialUnits[i].childNodes[3].textContent;
+    var td5 = document.createElement('td');
+    td5.className = 'Period';
+    td5.id = 'td5_row_'+i;
+    td5.textContent = itemPeriod;
+    document.getElementById('row_'+i).appendChild(td5);
   };
 
 };
